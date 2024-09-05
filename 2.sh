ceee6f25f0dd5d7e51c77a17443325d5a48f41f6
Ngducok <127043162+Ngducok@users.noreply.github.com>
2024-09-05 14:16:54 +0700

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,11 @@
+pkg update -y && pkg upgrade -y
+
+termux-change-repo
+
+pkg install python
+
+pip install --upgrade pip
+
+pip install subprocess time threading signal os sys requests colorama pystyle
+
+echo "downloaded successfully"
